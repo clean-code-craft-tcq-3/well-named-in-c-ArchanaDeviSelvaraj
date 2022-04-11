a8616d6593d1294e53d7756fc65f335dc5cb3c15
ArchanaDeviSelvaraj <100116449+ArchanaDeviSelvaraj@users.noreply.github.com>
2022-04-11 19:56:20 +0530

    Update main.c

--- main.c
+++ main.c
@@ -3,18 +3,11 @@
 
 enum MajorColor {WHITE, RED, BLACK, YELLOW, VIOLET};
 enum MinorColor {BLUE, ORANGE, GREEN, BROWN, SLATE};
-
-const char* MajorColorNames[] = {
-    "White", "Red", "Black", "Yellow", "Violet"
-};
-int numberOfMajorColors =
-    sizeof(MajorColorNames) / sizeof(MajorColorNames[0]);
-const char* MinorColorNames[] = {
-    "Blue", "Orange", "Green", "Brown", "Slate"
-};
+const char* MajorColorNames[] = {"White", "Red", "Black", "Yellow", "Violet"};
+int numberOfMajorColors = sizeof(MajorColorNames) / sizeof(MajorColorNames[0]);
+const char* MinorColorNames[] = {"Blue", "Orange", "Green", "Brown", "Slate"};
 const int MAX_COLORPAIR_NAME_CHARS = 16;
-int numberOfMinorColors =
-    sizeof(MinorColorNames) / sizeof(MinorColorNames[0]);
+int numberOfMinorColors = sizeof(MinorColorNames) / sizeof(MinorColorNames[0]);
 
 typedef struct {
     enum MajorColor majorColor;
@@ -24,9 +17,7 @@ typedef struct {
 int main() {
     testNumberToPair(4, WHITE, BROWN);
     testNumberToPair(5, WHITE, SLATE);
-
     testPairToNumber(BLACK, ORANGE, 12);
     testPairToNumber(VIOLET, SLATE, 25);
-
     return 0;
 }
