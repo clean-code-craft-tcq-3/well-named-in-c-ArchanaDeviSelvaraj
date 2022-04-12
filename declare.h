09a11cffd2e3dd2d6fe414744c95baa6b8f7f2a4
ArchanaDeviSelvaraj <100116449+ArchanaDeviSelvaraj@users.noreply.github.com>
2022-04-12 19:40:24 +0530

    Update declare.h

--- declare.h
+++ declare.h
@@ -1,5 +1,1 @@
-void ColorPairToString(const ColorPair* , char*);
-ColorPair GetColorFromPairNumber(int);
-int GetPairNumberFromColor(const ColorPair* );
-void testNumberToPair(int ,enum MajorColor ,enum MinorColor );
-void testPairToNumber(enum MajorColor ,enum MinorColor ,int );
+
