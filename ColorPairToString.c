46d66d509c5fa38ec28b1651d295bf11ee7fefd3
ArchanaDeviSelvaraj <100116449+ArchanaDeviSelvaraj@users.noreply.github.com>
2022-04-12 19:46:51 +0530

    Update ColorPairToString.c

--- ColorPairToString.c
+++ ColorPairToString.c
@@ -1,4 +1,6 @@
 #include <stdio.h>
+enum MajorColor {WHITE, RED, BLACK, YELLOW, VIOLET};
+enum MinorColor {BLUE, ORANGE, GREEN, BROWN, SLATE};
 typedef struct {
     enum MajorColor majorColor;
     enum MinorColor minorColor;
