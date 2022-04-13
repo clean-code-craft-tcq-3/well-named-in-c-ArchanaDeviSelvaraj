d31834f16318e66aa05c4d9fb36ce419592378e3
ArchanaDeviSelvaraj <100116449+ArchanaDeviSelvaraj@users.noreply.github.com>
2022-04-13 20:34:06 +0530

    Update declare.h

--- declare.h
+++ declare.h
@@ -1,10 +1,3 @@
-#include <stdio.h>
-#include <assert.h>
-    
-const char* MajorColorNames[] = {"White", "Red", "Black", "Yellow", "Violet"};
-int numberOfMajorColors = sizeof(MajorColorNames) / sizeof(MajorColorNames[0]);
-const char* MinorColorNames[] = {"Blue", "Orange", "Green", "Brown", "Slate"};
-const int MAX_COLORPAIR_NAME_CHARS = 16;
-int numberOfMinorColors = sizeof(MinorColorNames) / sizeof(MinorColorNames[0]);
+
 
 
