91f6a372ba6d0b90537f3e90cff0f46edfc3bd48
ArchanaDeviSelvaraj <100116449+ArchanaDeviSelvaraj@users.noreply.github.com>
2022-04-13 20:22:32 +0530

    Update GetPairNumberFromColor.c

--- GetPairNumberFromColor.c
+++ GetPairNumberFromColor.c
@@ -1,4 +1,12 @@
 #include "declare.h"
+
+    enum MajorColor {WHITE, RED, BLACK, YELLOW, VIOLET};
+    enum MinorColor {BLUE, ORANGE, GREEN, BROWN, SLATE};
+    
+    typedef struct {
+    enum MajorColor majorColor;
+    enum MinorColor minorColor;
+    } ColorPair;
 int GetPairNumberFromColor(const ColorPair* colorPair) {
     return colorPair->majorColor * numberOfMinorColors +
             colorPair->minorColor + 1;
