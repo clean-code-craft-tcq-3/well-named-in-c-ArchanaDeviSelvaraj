95c1ba4a17d82af259383d3394a7a9e2ab4ae617
ArchanaDeviSelvaraj <100116449+ArchanaDeviSelvaraj@users.noreply.github.com>
2022-04-13 20:43:48 +0530

    Update testPairToNumber.c

--- testPairToNumber.c
+++ testPairToNumber.c
@@ -1,19 +1,6 @@
 #include "GetPairNumberFromColor.c"
-    enum MajorColor {WHITE, RED, BLACK, YELLOW, VIOLET};
-    enum MinorColor {BLUE, ORANGE, GREEN, BROWN, SLATE};
-    
-    typedef struct {
-    enum MajorColor majorColor;
-    enum MinorColor minorColor;
-    } ColorPair;
-
-    enum MajorColor {WHITE, RED, BLACK, YELLOW, VIOLET};
-    enum MinorColor {BLUE, ORANGE, GREEN, BROWN, SLATE};
-    
-    typedef struct {
-    enum MajorColor majorColor;
-    enum MinorColor minorColor;
-    } ColorPair;
+#include <assert.h>
+   
 void testPairToNumber(enum MajorColor major,enum MinorColor minor,int expectedPairNumber)
 {
     ColorPair colorPair;
