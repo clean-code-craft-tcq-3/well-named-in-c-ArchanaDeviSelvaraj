76815775f6a65fcc75234e572ed4e32bf466b335
ArchanaDeviSelvaraj <100116449+ArchanaDeviSelvaraj@users.noreply.github.com>
2022-04-13 19:52:57 +0530

    Update declare.h

--- declare.h
+++ declare.h
@@ -19,3 +19,9 @@ typedef struct {
     enum MajorColor majorColor;
     enum MinorColor minorColor;
 } ColorPair;
+
+void ColorPairToString(const ColorPair* , char*);
+ColorPair GetColorFromPairNumber(int);
+int GetPairNumberFromColor(const ColorPair* );
+void testNumberToPair(int ,enum MajorColor ,enum MinorColor );
+void testPairToNumber(enum MajorColor ,enum MinorColor ,int );
