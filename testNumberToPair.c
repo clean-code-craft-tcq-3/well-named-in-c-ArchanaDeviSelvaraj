ced7466a1048d98c946d16eeba550acc8efd1254
ArchanaDeviSelvaraj <100116449+ArchanaDeviSelvaraj@users.noreply.github.com>
2022-04-13 20:25:59 +0530

    Update testNumberToPair.c

--- testNumberToPair.c
+++ testNumberToPair.c
@@ -1,4 +1,5 @@
-#include "declare.h"
+#include "declare.h".
+#include "GetColorFromPairNumber.c"
 
     enum MajorColor {WHITE, RED, BLACK, YELLOW, VIOLET};
     enum MinorColor {BLUE, ORANGE, GREEN, BROWN, SLATE};
