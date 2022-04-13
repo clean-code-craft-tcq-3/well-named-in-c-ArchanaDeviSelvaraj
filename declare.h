adb5afd019bd0144b984da9cf2e781f3fb524f9c
ArchanaDeviSelvaraj <100116449+ArchanaDeviSelvaraj@users.noreply.github.com>
2022-04-13 20:09:04 +0530

    Update declare.h

--- declare.h
+++ declare.h
@@ -7,10 +7,6 @@ const char* MinorColorNames[] = {"Blue", "Orange", "Green", "Brown", "Slate"};
 const int MAX_COLORPAIR_NAME_CHARS = 16;
 int numberOfMinorColors = sizeof(MinorColorNames) / sizeof(MinorColorNames[0]);
 
-typedef struct {
-    enum MajorColor majorColor;
-    enum MinorColor minorColor;
-} ColorPair;
 void ColorPairToString(const ColorPair* , char*);
 ColorPair GetColorFromPairNumber(int);
 int GetPairNumberFromColor(const ColorPair* );
