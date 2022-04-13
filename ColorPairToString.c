29767b6b9295fdc5a22632592034492e6e0ba6d9
ArchanaDeviSelvaraj <100116449+ArchanaDeviSelvaraj@users.noreply.github.com>
2022-04-13 20:20:02 +0530

    Update ColorPairToString.c

--- ColorPairToString.c
+++ ColorPairToString.c
@@ -1,7 +1,13 @@
 #include "declare.h"
 
+    enum MajorColor {WHITE, RED, BLACK, YELLOW, VIOLET};
+    enum MinorColor {BLUE, ORANGE, GREEN, BROWN, SLATE};
+    
+    typedef struct {
+    enum MajorColor majorColor;
+    enum MinorColor minorColor;
+    } ColorPair;
+
 void ColorPairToString(const ColorPair* colorPair, char* buffer) {
-    sprintf(buffer, "%s %s",
-        MajorColorNames[colorPair->majorColor],
-        MinorColorNames[colorPair->minorColor]);
+    sprintf(buffer, "%s %s",MajorColorNames[colorPair->majorColor],MinorColorNames[colorPair->minorColor]);
 }
