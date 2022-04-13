fa6107446162d246e62b29432ed8459946eabedd
ArchanaDeviSelvaraj <100116449+ArchanaDeviSelvaraj@users.noreply.github.com>
2022-04-13 20:12:51 +0530

    Update main.c

--- main.c
+++ main.c
@@ -2,11 +2,7 @@
 #include <assert.h>
 #include "declare.h"
 
-
-
-int main() {
-
-    enum MajorColor {WHITE, RED, BLACK, YELLOW, VIOLET};
+enum MajorColor {WHITE, RED, BLACK, YELLOW, VIOLET};
     enum MinorColor {BLUE, ORANGE, GREEN, BROWN, SLATE};
     
     typedef struct {
@@ -19,6 +15,11 @@ int main() {
     int GetPairNumberFromColor(const ColorPair* );
     void testNumberToPair(int ,enum MajorColor ,enum MinorColor );
     void testPairToNumber(enum MajorColor ,enum MinorColor ,int );
+
+
+int main() {
+
+    
     testNumberToPair(4, WHITE, BROWN);
     testNumberToPair(5, WHITE, SLATE);
 
