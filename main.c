ede8a6ded2b50458c7a241ebdbaf6cd9a63fe5de
ArchanaDeviSelvaraj <100116449+ArchanaDeviSelvaraj@users.noreply.github.com>
2022-04-13 20:09:37 +0530

    Update main.c

--- main.c
+++ main.c
@@ -8,6 +8,11 @@ int main() {
 
     enum MajorColor {WHITE, RED, BLACK, YELLOW, VIOLET};
     enum MinorColor {BLUE, ORANGE, GREEN, BROWN, SLATE};
+    
+    typedef struct {
+    enum MajorColor majorColor;
+    enum MinorColor minorColor;
+    } ColorPair;
     testNumberToPair(4, WHITE, BROWN);
     testNumberToPair(5, WHITE, SLATE);
 
