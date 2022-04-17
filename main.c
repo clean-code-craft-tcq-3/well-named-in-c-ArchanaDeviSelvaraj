3b4c782fbcb1eba4df7a674f8fe8d0aa5006a3bd
ArchanaDeviSelvaraj <100116449+ArchanaDeviSelvaraj@users.noreply.github.com>
2022-04-17 21:24:18 +0530

    Update main.c

--- main.c
+++ main.c
@@ -9,6 +9,12 @@
     enum MajorColor majorColor;
     enum MinorColor minorColor;
     } ColorPair;
+    const char* MajorColorNames[] = {"White", "Red", "Black", "Yellow", "Violet"};
+    const char* MinorColorNames[] = {"Blue", "Orange", "Green", "Brown", "Slate"};
+    int numberOfMajorColors = sizeof(MajorColorNames) / sizeof(MajorColorNames[0]);
+    int numberOfMinorColors = sizeof(MinorColorNames) / sizeof(MinorColorNames[0]);
+    const int MAX_COLORPAIR_NAME_CHARS = 16;
+   
     
     void ColorPairToString(const ColorPair* , char*);
     ColorPair GetColorFromPairNumber(int);
