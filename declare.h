3857c57cde21086afdc8acf8af0fc4ad66ddc59d
ArchanaDeviSelvaraj <100116449+ArchanaDeviSelvaraj@users.noreply.github.com>
2022-04-17 22:23:49 +0530

    Update declare.h

--- declare.h
+++ declare.h
@@ -1,4 +1,8 @@
     enum MajorColor {WHITE, RED, BLACK, YELLOW, VIOLET};
     enum MinorColor {BLUE, ORANGE, GREEN, BROWN, SLATE};
+    typedef struct {
+    enum MajorColor majorColor;
+    enum MinorColor minorColor;
+    } ColorPair;
 
 
