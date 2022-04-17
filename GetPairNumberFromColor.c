0ee2ea16b3fc2a60f37cd59bd04b0500ef6b6fad
ArchanaDeviSelvaraj <100116449+ArchanaDeviSelvaraj@users.noreply.github.com>
2022-04-17 22:31:40 +0530

    Update GetPairNumberFromColor.c

--- GetPairNumberFromColor.c
+++ GetPairNumberFromColor.c
@@ -2,9 +2,7 @@
 #include "declare.h"
 
 int GetPairNumberFromColor(const ColorPair* colorPair) {
-const char* MajorColorNames[] = {"White", "Red", "Black", "Yellow", "Violet"};
 const char* MinorColorNames[] = {"Blue", "Orange", "Green", "Brown", "Slate"};
-const int MAX_COLORPAIR_NAME_CHARS = 16;
 int numberOfMinorColors = sizeof(MinorColorNames) / sizeof(MinorColorNames[0]);
  return colorPair->majorColor * numberOfMinorColors + colorPair->minorColor + 1;
 }
