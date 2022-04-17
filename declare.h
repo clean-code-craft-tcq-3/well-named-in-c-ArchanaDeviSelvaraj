8633fd61d6d1ab43c753dc9dce0d42acdf002ffd
ArchanaDeviSelvaraj <100116449+ArchanaDeviSelvaraj@users.noreply.github.com>
2022-04-17 22:40:58 +0530

    Update declare.h

--- declare.h
+++ declare.h
@@ -4,5 +4,10 @@
     enum MajorColor majorColor;
     enum MinorColor minorColor;
     } ColorPair;
+    void ColorPairToString(const ColorPair* , char*);
+    ColorPair GetColorFromPairNumber(int);
+    int GetPairNumberFromColor(const ColorPair* );
+    void testNumberToPair(int ,enum MajorColor ,enum MinorColor );
+    void testPairToNumber(enum MajorColor ,enum MinorColor ,int );
 
 
