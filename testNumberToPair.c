363a6593fc565965d6756c735c0bcd78b8d17bda
ArchanaDeviSelvaraj <100116449+ArchanaDeviSelvaraj@users.noreply.github.com>
2022-04-17 22:38:31 +0530

    Update testNumberToPair.c

--- testNumberToPair.c
+++ testNumberToPair.c
@@ -1,6 +1,9 @@
 #include <assert.h>
 #include <stdio.h>
 #include "declare.h"
+void ColorPairToString(const ColorPair* , char*);
+ColorPair GetColorFromPairNumber(int);
+    
 void testNumberToPair(int pairNumber,enum MajorColor expectedMajor,enum MinorColor expectedMinor)
 {
 const int MAX_COLORPAIR_NAME_CHARS = 16;
