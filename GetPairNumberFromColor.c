1109706041d29ba84b7374f58cdb949def567bb3
ArchanaDeviSelvaraj <100116449+ArchanaDeviSelvaraj@users.noreply.github.com>
2022-04-17 21:49:55 +0530

    Update GetPairNumberFromColor.c

--- GetPairNumberFromColor.c
+++ GetPairNumberFromColor.c
@@ -1,15 +1,25 @@
 #include <stdio.h>
-//const char* MajorColorNames[] = {"White", "Red", "Black", "Yellow", "Violet"};
-//const char* MinorColorNames[] = {"Blue", "Orange", "Green", "Brown", "Slate"};
-//int numberOfMajorColors = sizeof(MajorColorNames) / sizeof(MajorColorNames[0]);
-//int numberOfMinorColors = sizeof(MinorColorNames) / sizeof(MinorColorNames[0]);
-    //enum MajorColor {WHITE, RED, BLACK, YELLOW, VIOLET};
-    //enum MinorColor {BLUE, ORANGE, GREEN, BROWN, SLATE};
-    
-    /*typedef struct {
+
+int GetPairNumberFromColor(const ColorPair* colorPair) {
+    enum MajorColor {WHITE, RED, BLACK, YELLOW, VIOLET};
+enum MinorColor {BLUE, ORANGE, GREEN, BROWN, SLATE};
+
+const char* MajorColorNames[] = {
+    "White", "Red", "Black", "Yellow", "Violet"
+};
+int numberOfMajorColors =
+    sizeof(MajorColorNames) / sizeof(MajorColorNames[0]);
+const char* MinorColorNames[] = {
+    "Blue", "Orange", "Green", "Brown", "Slate"
+};
+const int MAX_COLORPAIR_NAME_CHARS = 16;
+int numberOfMinorColors =
+    sizeof(MinorColorNames) / sizeof(MinorColorNames[0]);
+
+typedef struct {
     enum MajorColor majorColor;
     enum MinorColor minorColor;
-    } ColorPair;*/
-int GetPairNumberFromColor(const ColorPair* ColorPair) {
-    return ColorPair->majorColor * numberOfMinorColors + ColorPair->minorColor + 1;
+} ColorPair;
+    return colorPair->majorColor * numberOfMinorColors +
+            colorPair->minorColor + 1;
 }
