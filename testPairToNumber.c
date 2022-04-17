43c1f1e5254395ecdbf770b50bdeb19e1a77ea76
ArchanaDeviSelvaraj <100116449+ArchanaDeviSelvaraj@users.noreply.github.com>
2022-04-17 22:28:43 +0530

    Update testPairToNumber.c

--- testPairToNumber.c
+++ testPairToNumber.c
@@ -1,18 +1,11 @@
 #include <assert.h>
 #include <stdio.h>
+#include "declare.h"
    
 void testPairToNumber(enum MajorColor major,enum MinorColor minor,int expectedPairNumber)
 {
-    
-   enum MajorColor {WHITE, RED, BLACK, YELLOW, VIOLET};
-   enum MinorColor {BLUE, ORANGE, GREEN, BROWN, SLATE};
-
 const char* MajorColorNames[] = {"White", "Red", "Black", "Yellow", "Violet"};
 const char* MinorColorNames[] = {"Blue", "Orange", "Green", "Brown", "Slate"};
-typedef struct {
-    enum MajorColor majorColor;
-    enum MinorColor minorColor;
-} ColorPair;
 ColorPair colorPair;
     colorPair.majorColor = major;
     colorPair.minorColor = minor;
