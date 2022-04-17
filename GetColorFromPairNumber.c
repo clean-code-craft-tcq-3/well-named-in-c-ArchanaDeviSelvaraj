6d55f12dcf476265e6128b7dea5314c4be4a9698
ArchanaDeviSelvaraj <100116449+ArchanaDeviSelvaraj@users.noreply.github.com>
2022-04-17 21:22:01 +0530

    Update GetColorFromPairNumber.c

--- GetColorFromPairNumber.c
+++ GetColorFromPairNumber.c
@@ -1,15 +1,15 @@
 #include <stdio.h>
-const char* MajorColorNames[] = {"White", "Red", "Black", "Yellow", "Violet"};
-const char* MinorColorNames[] = {"Blue", "Orange", "Green", "Brown", "Slate"};
-int numberOfMajorColors = sizeof(MajorColorNames) / sizeof(MajorColorNames[0]);
-int numberOfMinorColors = sizeof(MinorColorNames) / sizeof(MinorColorNames[0]);
-    enum MajorColor {WHITE, RED, BLACK, YELLOW, VIOLET};
-    enum MinorColor {BLUE, ORANGE, GREEN, BROWN, SLATE};
+//const char* MajorColorNames[] = {"White", "Red", "Black", "Yellow", "Violet"};
+//const char* MinorColorNames[] = {"Blue", "Orange", "Green", "Brown", "Slate"};
+//int numberOfMajorColors = sizeof(MajorColorNames) / sizeof(MajorColorNames[0]);
+//int numberOfMinorColors = sizeof(MinorColorNames) / sizeof(MinorColorNames[0]);
+    //enum MajorColor {WHITE, RED, BLACK, YELLOW, VIOLET};
+    //enum MinorColor {BLUE, ORANGE, GREEN, BROWN, SLATE};
     
-    typedef struct {
+    /*typedef struct {
     enum MajorColor majorColor;
     enum MinorColor minorColor;
-    } ColorPair;
+    } ColorPair;*/
 ColorPair GetColorFromPairNumber(int pairNumber) {
     ColorPair colorPair;
     int zeroBasedPairNumber = pairNumber - 1;
