0ddd1b14542ff532916188d12f55ba002dd3c336
ArchanaDeviSelvaraj <100116449+ArchanaDeviSelvaraj@users.noreply.github.com>
2022-04-18 11:11:04 +0530

    Update MapColornamesPairnumber.c

--- MapColornamesPairnumber.c
+++ MapColornamesPairnumber.c
@@ -1,9 +1,14 @@
 #include "declare.h"
+#include "stdio.h"
 
 void MapColornamesPairnumber()
 {
   for (int i=1; i<=25 ; i++)
   {
-   ColorPair GetColorFromPairNumber(i);
+   const int MAX_COLORPAIR_NAME_CHARS = 16;
+   ColorPair colorPair = GetColorFromPairNumber(i);
+   char colorPairNames[MAX_COLORPAIR_NAME_CHARS];
+   ColorPairToString(&colorPair, colorPairNames);
+   printf("Pair number:%d:%s\n", i,colorPairNames);
   }
 }
