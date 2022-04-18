6949328b1f79f12ec9d69e083dec7f57719f4383
ArchanaDeviSelvaraj <100116449+ArchanaDeviSelvaraj@users.noreply.github.com>
2022-04-18 10:52:43 +0530

    Update MapColornamesPairnumber.c

--- MapColornamesPairnumber.c
+++ MapColornamesPairnumber.c
@@ -1,1 +1,14 @@
-
+#include "declare.h>
+void MapColornamesPairnumber()
+{
+  const char* MajorColorNames[] = {"White", "Red", "Black", "Yellow", "Violet"};
+  const char* MinorColorNames[] = {"Blue", "Orange", "Green", "Brown", "Slate"};
+  for (int i=1; i<=25 ; i++)
+  {
+    for (int j=1; j<=5 ; j++)
+    {
+      for (int k=1; k<=5 ; k++)
+    testNumberToPair(i, MajorColorNames[j], MinorColorNames[k]);
+    }
+  }
+}
