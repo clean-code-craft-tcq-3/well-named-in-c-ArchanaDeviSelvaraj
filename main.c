338b31b294ece72852633dc9606881160120ccbc
ArchanaDeviSelvaraj <100116449+ArchanaDeviSelvaraj@users.noreply.github.com>
2022-04-22 18:07:10 +0530

    Update main.c

--- main.c
+++ main.c
@@ -14,6 +14,6 @@ int main() {
     testNumberToPair(5, WHITE, SLATE);
     testPairToNumber(BLACK, ORANGE, 12);
     testPairToNumber(VIOLET, SLATE, 25);
-    MapColornamestoPairnumber();
+    PrintReferenceManual();
     return 0;
 }
