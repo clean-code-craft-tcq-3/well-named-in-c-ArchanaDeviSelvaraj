cb04f8868db850aaf1ba745dce7ad3d1631c48ef
ArchanaDeviSelvaraj <100116449+ArchanaDeviSelvaraj@users.noreply.github.com>
2022-04-22 18:57:05 +0530

    Update ColorCoding.h

--- ColorCoding.h
+++ ColorCoding.h
@@ -4,6 +4,11 @@
     enum MajorColor majorColor;
     enum MinorColor minorColor;
     } ColorPair;
+    extern const char* MajorColorNames[] = {"White", "Red", "Black", "Yellow", "Violet"};
+    extern const char* MinorColorNames[] = {"Blue", "Orange", "Green", "Brown", "Slate"};
+    extern int numberOfMajorColors = sizeof(MajorColorNames) / sizeof(MajorColorNames[0]);
+    extern int numberOfMinorColors = sizeof(MinorColorNames) / sizeof(MinorColorNames[0]);
+    extern const int MAX_COLORPAIR_NAME_CHARS = 16;
     void ColorPairToString(const ColorPair* , char*);
     ColorPair GetColorFromPairNumber(int);
     int GetPairNumberFromColor(const ColorPair* );
