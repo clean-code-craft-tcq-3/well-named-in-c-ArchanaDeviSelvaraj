e3b67cef4a98f13366ff8770e58d8cc8c7cb6d5e
ArchanaDeviSelvaraj <100116449+ArchanaDeviSelvaraj@users.noreply.github.com>
2022-04-22 18:03:24 +0530

    Update main.c

--- main.c
+++ main.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
 #include <assert.h>
-#include "declare.h"
+#include "ColorCoding.h"
 
     const char* MajorColorNames[] = {"White", "Red", "Black", "Yellow", "Violet"};
     const char* MinorColorNames[] = {"Blue", "Orange", "Green", "Brown", "Slate"};
