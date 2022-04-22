b92dceb7936c7a3e6f4452815daba150908ad4ba
ArchanaDeviSelvaraj <100116449+ArchanaDeviSelvaraj@users.noreply.github.com>
2022-04-22 18:17:10 +0530

    Update PrintReferenceManual.c

--- PrintReferenceManual.c
+++ PrintReferenceManual.c
@@ -2,6 +2,10 @@
 #include <stdio.h>
 void PrintReferenceManual()
 {
+  const char* MajorColorNames[] = {"White", "Red", "Black", "Yellow", "Violet"};
+  const char* MinorColorNames[] = {"Blue", "Orange", "Green", "Brown", "Slate"};
+  int numberOfMajorColors = sizeof(MajorColorNames) / sizeof(MajorColorNames[0]);
+  int numberOfMinorColors = sizeof(MinorColorNames) / sizeof(MinorColorNames[0]);
   int maxpairnumber = numberOfMajorColors * numberOfMinorColors;
   for (int pairNumber=1; pairNumber<=maxpairNumber ; pairNumber++)
   {
