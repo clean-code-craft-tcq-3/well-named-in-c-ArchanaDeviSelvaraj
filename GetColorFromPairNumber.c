37535eb0c3c8f5db7f881e109423cc0b1ad67e59
ArchanaDeviSelvaraj <100116449+ArchanaDeviSelvaraj@users.noreply.github.com>
2022-04-22 19:01:35 +0530

    Update GetColorFromPairNumber.c

--- GetColorFromPairNumber.c
+++ GetColorFromPairNumber.c
@@ -2,8 +2,6 @@
 #include "ColorCoding.h"
 ColorPair GetColorFromPairNumber(int pairNumber) 
 {
-const char* MinorColorNames[] = {"Blue", "Orange", "Green", "Brown", "Slate"};
-int numberOfMinorColors = sizeof(MinorColorNames) / sizeof(MinorColorNames[0]);
     ColorPair colorPair;
     int zeroBasedPairNumber = pairNumber - 1;
     colorPair.majorColor = (enum MajorColor)(zeroBasedPairNumber / numberOfMinorColors);
