9ea58e67ca38a517259d6d201aa3c586b18afcd1
ArchanaDeviSelvaraj <100116449+ArchanaDeviSelvaraj@users.noreply.github.com>
2022-04-22 19:00:34 +0530

    Update ColorPairToString.c

--- ColorPairToString.c
+++ ColorPairToString.c
@@ -2,7 +2,7 @@
 #include "ColorCoding.h"
 void ColorPairToString(const ColorPair* colorPair, char* buffer) 
 {
-const char* MajorColorNames[] = {"White", "Red", "Black", "Yellow", "Violet"};
-const char* MinorColorNames[] = {"Blue", "Orange", "Green", "Brown", "Slate"};
+//const char* MajorColorNames[] = {"White", "Red", "Black", "Yellow", "Violet"};
+//const char* MinorColorNames[] = {"Blue", "Orange", "Green", "Brown", "Slate"};
 sprintf(buffer, "%s %s",MajorColorNames[colorPair->majorColor],MinorColorNames[colorPair->minorColor]);
 }
