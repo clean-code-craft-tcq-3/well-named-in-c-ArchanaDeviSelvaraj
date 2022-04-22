0b68de9d310a1fd774ad20e5aedca55a406cdda9
ArchanaDeviSelvaraj <100116449+ArchanaDeviSelvaraj@users.noreply.github.com>
2022-04-22 18:10:50 +0530

    Update PrintReferenceManual.c

--- PrintReferenceManual.c
+++ PrintReferenceManual.c
@@ -2,7 +2,7 @@
 #include <stdio.h>
 void PrintReferenceManual()
 {
-  for (int i=1; i<=25 ; i++)
+  for (int pairNumber=1; pairNumber<=25 ; pairNumber++)
   {
    const int MAX_COLORPAIR_NAME_CHARS = 16;
    ColorPair colorPair = GetColorFromPairNumber(i);
